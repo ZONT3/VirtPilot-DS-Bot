ac732cec3eb7881cb653646988fdb997e030a2b0
ZONT_ <danya10001@gmail.com>
2021-05-01 17:56:19 +0300

    Иностранцы?

--- src/main/java/ru/vpilot/dsbot/listeners/Greetings.java
+++ src/main/java/ru/vpilot/dsbot/listeners/Greetings.java
@@ -13,14 +13,17 @@ import static ru.vpilot.dsbot.Strings.*;
 public class Greetings extends ListenerAdapter {
     public static final String ID_ROLE_CHECKED   = "809358190813773836";
     public static final String ID_ROLE_MEDIA     = "819138179722248193";
+    public static final String ID_ROLE_FGN       = "836898707793510400";
     public static final String ID_CHANNEL_CP     = "637552217762562050";
     public static final String ID_MSG_CP         = "814552497468473376";
     public static final String ID_MSG_MEDIA      = "826390523325579275";
+    public static final String ID_MSG_FGN        = "838065483395498028";
     public static final String ID_CHANNEL_CP_MSG = "814043680775471136";
     public static final String ID_GUILD          = "620965426381324288";
     public static final String EMOJI             = "U+1F6EC";
     private Role roleChecked;
     private Role roleMedia;
+    private Role roleFGN;
 
     @Override
     public void onGuildReady(@NotNull GuildReadyEvent event) { // TODO Instantiate all the shit
@@ -31,6 +34,7 @@ public class Greetings extends ListenerAdapter {
 
         roleChecked = event.getJDA().getRoleById(ID_ROLE_CHECKED);
         roleMedia = event.getJDA().getRoleById(ID_ROLE_MEDIA);
+        roleFGN = event.getJDA().getRoleById(ID_ROLE_FGN);
     }
 
     @Override
@@ -39,6 +43,7 @@ public class Greetings extends ListenerAdapter {
         switch (event.getMessageId()) {
             case ID_MSG_CP -> addRole(event.getGuild(), event.getMember(), roleChecked);
             case ID_MSG_MEDIA -> addRole(event.getGuild(), event.getMember(), roleMedia);
+            case ID_MSG_FGN -> addRole(event.getGuild(), event.getMember(), roleFGN);
         }
 
     }
@@ -49,6 +54,7 @@ public class Greetings extends ListenerAdapter {
         switch (event.getMessageId()) {
             case ID_MSG_CP -> rmRole(event.getGuild(), event.getMember(), roleChecked);
             case ID_MSG_MEDIA -> rmRole(event.getGuild(), event.getMember(), roleMedia);
+            case ID_MSG_FGN -> rmRole(event.getGuild(), event.getMember(), roleFGN);
         }
     }
 
